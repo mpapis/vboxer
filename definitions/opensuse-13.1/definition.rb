8df32c22d9328e2d1b5c91154f79efa78f990326
Michal Papis <mpapis@gmail.com>
2014-10-02 20:59:49 +0200

    use the new define_yaml to load multiple configuration files

--- definitions/opensuse-13.1/definition.rb
+++ definitions/opensuse-13.1/definition.rb
@@ -4,12 +4,4 @@ require 'yaml'
 # change opensuse_64_dvd to one of configurations in *.yml in this dir
 # use the yml files to configure
 #
-opensuse_type = :opensuse_64_dvd
-
-#
-# do not change bellow
-#
-opensuse_definition = YAML.load_file("definition.yml")
-opensuse_flavor     = YAML.load_file("#{opensuse_type}.yml")
-
-Veewee::Definition.declare(opensuse_definition.merge(opensuse_flavor))
+Veewee::Definition.declare_yaml('definition.yml', 'opensuse_64_dvd.yml')
